35305922e83d1b50e4974ea90e72da3845f32eaa
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2019-05-01 17:10:02 -0300

    Enhancing the test script

--- shell/performance-test.sh
+++ shell/performance-test.sh
@@ -6,6 +6,7 @@
 testTimes=30 # Quantas vezes testar?
 testTimeout=3 # Resposta em até quantos segundos?
 testSleep=0.1 # Quanto tempo esperar entre cada tentativa?
+testURL="http://qconsp-test.lowbit.com.br" # Qual URL será testada?
 
 echo "Ultimate Performance Test !!!"
 echo "  (hint: Don't use it for real ^^)"
@@ -14,14 +15,14 @@ echo "Test Parameters:"
 echo "  Number of tests:    ${testTimes} test(s)"
 echo "  Maximum timeout:    ${testTimeout} second(s)"
 echo "  Time between tests: ${testSleep} second(s)"
+echo "  URL to be tested:   ${testURL}"
 echo
 echo "Starting tests..."
 
 for testRun in $(seq 1 1 ${testTimes})
 do
   echo -n "  -> Attempt ${testRun} of ${testTimes}: "
-  timeout ${testTimeout} curl -s http://qconsp-test.lowbit.com.br > /dev/null 2>&1
-  #timeout ${testTimeout} curl -s http://${external_ip} > /dev/null 2>&1
+  timeout ${testTimeout} curl -s ${testURL} > /dev/null 2>&1
   curlRC=$?
 
   if [[ $curlRC -eq 0 ]]; then
